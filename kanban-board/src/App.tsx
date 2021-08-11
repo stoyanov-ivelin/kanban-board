e43bf99fddbdedfe58c2f3810af366966df5ee2c
Ivelin Stoyanov <ivelin.stoyanov@atscale.com>
2021-08-11 13:59:56 +0300

    Add 3 Boards and filteredIssues to App.tsx

--- kanban-board/src/App.tsx
+++ kanban-board/src/App.tsx
@@ -1,18 +1,74 @@
-import React, { Component, ReactNode } from 'react';
-import { connect } from 'react-redux';
-import './App.css';
+import { AppBar, Toolbar, Typography } from "@material-ui/core";
+import { Component, ReactNode } from "react";
+import { connect } from "react-redux";
+import "./App.css";
+import { Status } from "./common/constants";
+import Board from "./components/Board/Board";
+import AccountCircle from "@material-ui/icons/AccountCircle";
+import { RootState } from "./store/store";
+import { IIssue } from "./common/types";
 
-class App extends Component {
+type Acc = {
+  new: Array<IIssue>;
+  inProgress: Array<IIssue>;
+  done: Array<IIssue>;
+};
+
+class App extends Component<RootState> {
   render(): ReactNode {
+    const filteredIssues = this.props.issues.reduce(
+      (acc: Acc, issue: IIssue) => {
+        if (issue.status === Status.NEW) {
+          acc.new.push(issue);
+        } else if (issue.status === Status.IN_PROGRESS) {
+          acc.inProgress.push(issue);
+        } else {
+          acc.done.push(issue);
+        }
+
+        return acc;
+      },
+      { new: [], inProgress: [], done: [] }
+    );
+
+    const newIssues = filteredIssues.new;
+    const inProgressIssues = filteredIssues.inProgress;
+    const doneIssues = filteredIssues.done;
+
     return (
-    <div className="App">
-      Hello, World!
-    </div>
-    )};
+      <div className="App">
+        <AppBar position="static">
+          <Toolbar style={{ left: "140em" }}>
+            <Typography variant="h5">User Name</Typography>
+            <AccountCircle style={{ marginLeft: "1em" }} />
+          </Toolbar>
+        </AppBar>
+        <h1 style={{ margin: "1.5em" }}>Kanban Board</h1>
+        <div className="columnGrid">
+          <Board
+            title={Status.NEW}
+            count={newIssues.length}
+            issues={newIssues}
+          />
+          <Board
+            title={Status.IN_PROGRESS}
+            count={inProgressIssues.length}
+            issues={inProgressIssues}
+          />
+          <Board
+            title={Status.DONE}
+            count={doneIssues.length}
+            issues={doneIssues}
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
-const mapStateToProps = (state: any) => ({
+const mapStateToProps = (state: RootState) => ({
   issues: state.issues,
+  currentDraggedElement: state.currentDraggedElement,
 });
 
 export default connect(mapStateToProps)(App);
