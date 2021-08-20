3dfd1e9c97e44ff41a75418dca63a911b87b14a3
Ivelin Stoyanov <ivelin.stoyanov@atscale.com>
2021-08-20 13:11:51 +0300

    Add editIssue action and action creator

--- src/common/actions.ts
+++ src/common/actions.ts
@@ -1,8 +1,10 @@
-import { UpdateStatusPayload, CreateIssuePayload } from "common/models";
+import { UpdateStatusPayload, CreateIssuePayload, EditIssuePayload } from "common/models";
 
 export const UPDATE_STATUS = "UPDATE_STATUS";
 export const CREATE_ISSUE = "CREATE_ISSUE";
+export const EDIT_ISSUE = "EDIT_ISSUE";
 
 
 export const updateStatus = (payload: UpdateStatusPayload) => ({type: UPDATE_STATUS, payload});
 export const createIssue = (payload: CreateIssuePayload) => ({type: CREATE_ISSUE, payload});
+export const editIssue = (payload: EditIssuePayload) => ({type: EDIT_ISSUE, payload});
