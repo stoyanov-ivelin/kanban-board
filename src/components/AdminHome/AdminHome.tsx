94b258d1a3dbd95034ec4b6dbe4a31ee99a311b6
Ivelin Stoyanov <ivelin.stoyanov@atscale.com>
2021-08-24 13:44:31 +0300

    Add AdminHome Page

---
+++ src/components/AdminHome/AdminHome.tsx
@@ -0,0 +1,36 @@
+import { Container, Grid, Typography, Button } from "@material-ui/core";
+import { AdminPanelLinks } from "common/constants";
+import "components/AdminHome/AdminHome.css";
+import SideNav from "components/SideNav/SideNav";
+import { NavLink } from "react-router-dom";
+
+const AdminHome = () => {
+  return (
+    <div className="admin-home">
+      <SideNav />
+      <Container>
+        <Typography variant="h3" style={{ margin: "100px" }}>
+          Administration Panel
+        </Typography>
+        <Grid container spacing={8}>
+          {AdminPanelLinks.map((link) => (
+            <Grid item xs={6}>
+              <Button
+                component={NavLink}
+                to={`/admin/${link.toLocaleLowerCase()}`}
+                fullWidth
+                variant="outlined"
+                classes={{ label: "admin-nav-button"}}
+                style={{borderWidth: "5px"}}
+              >
+                {link}
+              </Button>
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+    </div>
+  );
+};
+
+export default AdminHome;
