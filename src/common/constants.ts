599cf4137d573be777e13349b0685d45666387c6
Ivelin Stoyanov <ivelin.stoyanov@atscale.com>
2021-08-24 13:43:54 +0300

    Add AdminPanelLinks and SkillsImages constants

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -10,3 +10,14 @@ export const issueConstants = {
   titleErrorMsg: "Please enter a title between 3 and 50 characters!",
   assigneeErrorMsg: "Please select an assignee",
 }
+
+export const AdminPanelLinks = ["Users", "Boards", "Issue config", "Workflows"];
+
+export const SkillsImages = {
+  Java: "https://www.meme-arsenal.com/memes/aedccb7b888468d35e9855e503b8e706.jpg",
+  "C#": "https://e7.pngegg.com/pngimages/340/226/png-clipart-purple-and-white-logo-c-computer-programming-software-development-programmer-marklogic-coder-miscellaneous-purple.png",
+  Scala: "https://cdn.iconscout.com/icon/free/png-256/scala-226059.png",
+  GoLang: "https://img.icons8.com/color/452/golang.png",
+  JavaScript:
+    "https://cdn.iconscout.com/icon/free/png-512/javascript-2752148-2284965.png",
+};
