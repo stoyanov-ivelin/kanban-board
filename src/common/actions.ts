7bbfd7a61e7c774ef90697bbd10985fc474e4482
Ivelin Stoyanov <ivelin.stoyanov@atscale.com>
2021-08-26 10:01:10 +0300

    Add createUser action creator

--- src/common/actions.ts
+++ src/common/actions.ts
@@ -1,10 +1,12 @@
-import { UpdateStatusPayload, CreateIssuePayload, EditIssuePayload } from "common/models";
+import { UpdateStatusPayload, CreateIssuePayload, EditIssuePayload, CreateUserPayload } from "common/models";
 
 export const UPDATE_STATUS = "UPDATE_STATUS";
 export const CREATE_ISSUE = "CREATE_ISSUE";
 export const EDIT_ISSUE = "EDIT_ISSUE";
+export const CREATE_USER = "CREATE_USER";
 
 
 export const updateStatus = (payload: UpdateStatusPayload) => ({type: UPDATE_STATUS, payload});
 export const createIssue = (payload: CreateIssuePayload) => ({type: CREATE_ISSUE, payload});
 export const editIssue = (payload: EditIssuePayload) => ({type: EDIT_ISSUE, payload});
+export const createUser = (payload: CreateUserPayload) => ({type: CREATE_USER, payload});
