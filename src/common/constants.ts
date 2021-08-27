58e06529aa21c586cf2ea6bbdcc9d79d336c531c
Ivelin Stoyanov <ivelin.stoyanov@atscale.com>
2021-08-27 12:59:05 +0300

    Add Skills constant for all skills and their img

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -23,13 +23,26 @@ export const userConstants = {
 
 export const AdminPanelLinks = ["Users", "Boards", "Issue config", "Workflows"];
 
-export const skillsConstant = ["JavaScript", "C#", "GoLang", "Scala", "Java"];
+export const Skills = {
+  Java: {
+    name: "Java",
+    img: "https://www.meme-arsenal.com/memes/aedccb7b888468d35e9855e503b8e706.jpg"
+  },
+  "C#": {
+    name: "C#",
+    img: "https://e7.pngegg.com/pngimages/340/226/png-clipart-purple-and-white-logo-c-computer-programming-software-development-programmer-marklogic-coder-miscellaneous-purple.png"
+  },
+  Scala: {
+    name: "Scala",
+    img: "https://cdn.iconscout.com/icon/free/png-256/scala-226059.png",
+  },
+  GoLang: {
+    name: "GoLang",
+    img: "https://img.icons8.com/color/452/golang.png",
+  },
+  JavaScript: {
+    name: "JavaScript",
+    img: "https://cdn.iconscout.com/icon/free/png-512/javascript-2752148-2284965.png",
+  }
+}
 
-export const SkillsImages = {
-  Java: "https://www.meme-arsenal.com/memes/aedccb7b888468d35e9855e503b8e706.jpg",
-  "C#": "https://e7.pngegg.com/pngimages/340/226/png-clipart-purple-and-white-logo-c-computer-programming-software-development-programmer-marklogic-coder-miscellaneous-purple.png",
-  Scala: "https://cdn.iconscout.com/icon/free/png-256/scala-226059.png",
-  GoLang: "https://img.icons8.com/color/452/golang.png",
-  JavaScript:
-    "https://cdn.iconscout.com/icon/free/png-512/javascript-2752148-2284965.png",
-};
