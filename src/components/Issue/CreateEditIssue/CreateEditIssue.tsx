1cb826c85bff8528b635639e3dea0aff3e6d1c11
Ivelin Stoyanov <ivelin.stoyanov@atscale.com>
2021-08-27 13:02:18 +0300

    Refactor CreateEditIssue

--- src/components/Issue/CreateEditIssue/CreateEditIssue.tsx
+++ src/components/Issue/CreateEditIssue/CreateEditIssue.tsx
@@ -10,6 +10,7 @@ import {
   MenuItem,
   Select,
   IconButton,
+  InputLabel,
 } from "@material-ui/core";
 import React, { Component } from "react";
 import { connect } from "react-redux";
@@ -22,11 +23,10 @@ import EditIcon from "@material-ui/icons/Edit";
 
 interface CreateEditIssueState {
   open: boolean;
-  isSubmitting: boolean;
   title: string;
   description: string;
   assignee: string;
-  status?: Status;
+  status: Status;
   charactersLeft: number;
   titleError: string | null;
   descriptionError: string | null;
@@ -37,7 +37,6 @@ interface CreateEditIssueProps {
   users: Array<IUser>;
   issue?: IIssue;
   successAction: (issue: IIssue) => void;
-  cancelAction: any;
   isEditing?: boolean;
 }
 
@@ -48,18 +47,16 @@ class CreateEditIssue extends Component<
   constructor(props: CreateEditIssueProps) {
     super(props);
 
-    const { isEditing } = this.props;
     this.state = {
       open: false,
-      isSubmitting: false,
       title: "",
       description: "",
       assignee: "",
       status: Status.New,
       charactersLeft: issueConstants.descriptionMaxChars,
-      titleError: isEditing ? null : issueConstants.titleErrorMsg,
-      descriptionError: isEditing ? null : issueConstants.descriptionErrorMsg,
-      assigneeError: isEditing ? null : issueConstants.assigneeErrorMsg,
+      titleError: null,
+      descriptionError: null,
+      assigneeError: null,
     };
   }
 
@@ -70,31 +67,25 @@ class CreateEditIssue extends Component<
         title,
         description,
         assignee,
-        status: status as Status,
+        status,
         charactersLeft: issueConstants.descriptionMaxChars - description.length,
       });
     }
   }
 
   render(): JSX.Element {
-    const {
-      open,
-      isSubmitting,
-      title,
-      description,
-      assignee,
-      status,
-      charactersLeft,
-      titleError,
-      descriptionError,
-      assigneeError,
-    } = this.state;
-
-    const { isEditing } = this.props;
-
     return (
       <div>
-        {isEditing ? (
+        {this.renderButton()}
+        {this.renderDialog()}
+      </div>
+    );
+  }
+
+  renderButton(): JSX.Element {
+    return (
+      <>
+        {this.props.isEditing ? (
           <IconButton
             color="primary"
             size="small"
@@ -113,84 +104,134 @@ class CreateEditIssue extends Component<
             Create New Issue
           </Button>
         )}
-        <Dialog open={open} onClose={this.handleClose} fullWidth>
+      </>
+    );
+  }
+
+  renderTitleField(): JSX.Element {
+    const { title, titleError } = this.state;
+
+    return (
+      <>
+        <InputLabel className="input-title" htmlFor="title" required>
+          Title
+        </InputLabel>
+        <TextField
+          onChange={this.handleTitleChange}
+          defaultValue={title}
+          fullWidth
+          variant="outlined"
+          margin="normal"
+          id="title"
+          placeholder="Enter title..."
+          helperText={titleError}
+          error={titleError !== null}
+        />
+      </>
+    );
+  }
+
+  renderDescriptionField(): JSX.Element {
+    const { description, descriptionError, charactersLeft } = this.state;
+
+    return (
+      <>
+        <InputLabel className="input-title" htmlFor="description" required>
+          Description
+        </InputLabel>
+        <TextField
+          onChange={this.handleDescriptionChange}
+          defaultValue={description}
+          fullWidth
+          multiline
+          minRows={6}
+          variant="outlined"
+          margin="normal"
+          id="description"
+          placeholder="Enter description..."
+          helperText={
+            descriptionError
+              ? descriptionError
+              : `Max ${issueConstants.descriptionMaxChars} characters, ${charactersLeft} left`
+          }
+          error={descriptionError !== null}
+        />
+      </>
+    );
+  }
+
+  renderAssigneeDropDown(): JSX.Element {
+    const { assignee, assigneeError } = this.state;
+
+    return (
+      <>
+        <InputLabel className="select-label" required>
+          Assignee
+        </InputLabel>
+        <FormControl>
+          <Select
+            onChange={this.handleAssigneeChange}
+            defaultValue={assignee}
+            displayEmpty
+            variant="outlined"
+            value={assignee}
+            error={assigneeError !== null}
+          >
+            <MenuItem value="">
+              <em>--select assignee--</em>
+            </MenuItem>
+            {this.props.users.map((user) => (
+              <MenuItem key={user.id} value={user.name}>
+                {user.name}
+              </MenuItem>
+            ))}
+          </Select>
+          <FormHelperText error={assigneeError !== null}>
+            {assigneeError}
+          </FormHelperText>
+        </FormControl>
+      </>
+    );
+  }
+
+  renderStatusField(): JSX.Element {
+    const { status } = this.state;
+
+    return (
+      <>
+        <InputLabel className="select-label">Status</InputLabel>
+        <FormControl>
+          <Select
+            onChange={this.handleStatusChange}
+            defaultValue={status}
+            displayEmpty
+            variant="outlined"
+            value={status}
+          >
+            {Object.values(Status).map((status, index) => (
+              <MenuItem key={index} value={status}>
+                {status}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </>
+    );
+  }
+
+  renderDialog(): JSX.Element {
+    const { isEditing } = this.props;
+
+    return (
+        <Dialog open={this.state.open} onClose={this.handleClose} fullWidth>
           <DialogTitle classes={{ root: "dialog-title" }}>
             {isEditing ? "Edit Issue" : "Add New Issue"}
           </DialogTitle>
           <DialogContent>
-            <div className="input-title">Title</div>
-            <TextField
-              onChange={this.handleTitleChange}
-              defaultValue={title}
-              fullWidth
-              variant="outlined"
-              margin="normal"
-              name="Title"
-              placeholder="Enter title..."
-              helperText={isSubmitting && titleError}
-              error={isSubmitting && titleError !== null}
-            />
-            <div className="input-title">Description</div>
-            <TextField
-              onChange={this.handleDescriptionChange}
-              defaultValue={description}
-              fullWidth
-              multiline
-              minRows={6}
-              variant="outlined"
-              margin="normal"
-              name="name"
-              placeholder="Enter description..."
-              helperText={
-                isSubmitting && descriptionError
-                  ? descriptionError
-                  : `Max ${issueConstants.descriptionMaxChars} characters, ${charactersLeft} left`
-              }
-              error={isSubmitting && descriptionError !== null}
-            />
-            <div className="select-label">Assignee</div>
-            <FormControl>
-              <Select
-                onChange={this.handleAssigneeChange}
-                defaultValue={assignee}
-                displayEmpty
-                variant="outlined"
-                value={assignee}
-                error={isSubmitting && assigneeError !== null}
-              >
-                <MenuItem value="">
-                  <em>--select assignee--</em>
-                </MenuItem>
-                {this.props.users.map((user) => (
-                  <MenuItem key={user.id} value={user.name}>
-                    {user.name}
-                  </MenuItem>
-                ))}
-              </Select>
-              <FormHelperText error={isSubmitting && assigneeError !== null}>
-                {isSubmitting && assigneeError}
-              </FormHelperText>
-            </FormControl>
-            {isEditing && (
-              <div>
-                <div className="select-label">Status</div>
-                <FormControl>
-                  <Select
-                    onChange={this.handleStatusChange}
-                    defaultValue={status}
-                    displayEmpty
-                    variant="outlined"
-                    value={status}
-                  >
-                    {Object.values(Status).map((status, index) => (
-                      <MenuItem key={index} value={status}>
-                        {status}
-                      </MenuItem>
-                    ))}
-                  </Select>
-                </FormControl>
-              </div>
-            )}
+            {this.renderTitleField()}
+            {this.renderDescriptionField()}
+            {this.renderAssigneeDropDown()}
+            {isEditing && this.renderStatusField()}
           </DialogContent>
           <DialogActions>
             <Button onClick={this.handleClose} color="primary">
@@ -201,21 +242,11 @@ class CreateEditIssue extends Component<
             </Button>
           </DialogActions>
         </Dialog>
-      </div>
     );
   }
 
   handleSubmit = (): void => {
-    const { titleError, descriptionError, assigneeError } = this.state;
-
-    this.setState(
-      {
-        isSubmitting: true,
-      },
-      () => this.handleValidation()
-    );
-
-    const hasErrros = titleError || descriptionError || assigneeError;
+    const hasErrros = !this.handleValidation();
     if (hasErrros) {
       return;
     }
@@ -231,51 +262,63 @@ class CreateEditIssue extends Component<
       title,
       description,
       assignee,
-      status: status as string,
+      status,
     });
     this.handleClose();
   };
 
-  handleValidation = (): void => {
-    this.handleTitleValidation();
-    this.handleDescriptionValidation();
-    this.handleAssigneeValidation();
+  handleValidation = (): boolean => {
+    const hasTtitleError = this.handleTitleValidation();
+    const hasDescriptionError = this.handleDescriptionValidation();
+    const hasAssigneeError = this.handleAssigneeValidation();
+
+    if (hasTtitleError || hasDescriptionError || hasAssigneeError) {
+      return false;
+    }
+
+    return true;
   };
 
-  handleTitleValidation = (): void => {
+  handleTitleValidation = (): boolean => {
     const { title } = this.state;
-    const isValid = !title || title.length < 3 || title.length > 50;
+    const isNotValid = !title || title.length < 3 || title.length > 50;
 
     this.setState({
-      titleError: isValid ? issueConstants.titleErrorMsg : null,
+      titleError: isNotValid ? issueConstants.titleErrorMsg : null,
     });
+
+    return isNotValid;
   };
 
-  handleDescriptionValidation = (): void => {
+  handleDescriptionValidation = (): boolean => {
     const { description } = this.state;
-    const isValid =
+    const isNotValid =
       !description ||
       description.length < 3 ||
       description.length > issueConstants.descriptionMaxChars;
 
     this.setState({
-      descriptionError: isValid ? issueConstants.descriptionErrorMsg : null,
+      descriptionError: isNotValid ? issueConstants.descriptionErrorMsg : null,
     });
+
+    return isNotValid;
   };
 
-  handleAssigneeValidation = (): void => {
+  handleAssigneeValidation = (): boolean => {
     const { assignee } = this.state;
-    const isValid = !assignee;
+    const isNotValid = !assignee;
 
     this.setState({
-      assigneeError: isValid ? issueConstants.assigneeErrorMsg : null,
+      assigneeError: isNotValid ? issueConstants.assigneeErrorMsg : null,
     });
+
+    return isNotValid;
   };
 
   handleTitleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
     const { value } = event.target;
 
-    this.setState({ title: value }, () => this.handleTitleValidation());
+    this.setState({ title: value, titleError: null });
   };
 
   handleDescriptionChange = (
@@ -283,13 +326,11 @@ class CreateEditIssue extends Component<
   ): void => {
     const { value } = event.target;
 
-    this.setState(
-      {
-        description: value,
-        charactersLeft: issueConstants.descriptionMaxChars - value.length,
-      },
-      () => this.handleDescriptionValidation()
-    );
+    this.setState({
+      description: value,
+      charactersLeft: issueConstants.descriptionMaxChars - value.length,
+      descriptionError: null,
+    });
   };
 
   handleAssigneeChange = (
@@ -297,34 +338,54 @@ class CreateEditIssue extends Component<
   ): void => {
     const { value } = event.target;
 
-    this.setState({ assignee: value as string }, () =>
-      this.handleAssigneeValidation()
-    );
+    this.setState({ assignee: value as string, assigneeError: null });
   };
 
-  handleStatusChange = (
-    event: React.ChangeEvent<{ name?: string | undefined; value: unknown }>
-  ): void => {
+  handleStatusChange = (event: React.ChangeEvent<{ value: unknown }>): void => {
     const { value } = event.target;
 
     this.setState({ status: value as Status });
   };
 
   handleClose = (): void => {
-    const { title, description, assignee, status } = this.state;
-
     if (this.props.isEditing) {
-      const stateAfterDialogClose = this.props.cancelAction(
-        title,
-        description,
-        assignee,
-        status
-      );
-      this.setState(stateAfterDialogClose);
+      this.handleCloseEditIssueDialog();
     } else {
-      const stateAfterDialogClose = this.props.cancelAction();
-      this.setState(stateAfterDialogClose);
+      this.handleCloseCreateIssueDialog();
     }
+  };
+
+  handleCloseCreateIssueDialog = (): void => {
+    const stateAfterDialogClose = {
+      open: false,
+      titleError: null,
+      descriptionError: null,
+      assigneeError: null,
+      charactersLeft: issueConstants.descriptionMaxChars,
+      title: "",
+      description: "",
+      assignee: "",
+    };
+
+    this.setState(stateAfterDialogClose);
+  };
+
+  handleCloseEditIssueDialog = (): void => {
+    const { title, description, assignee, status } = this.props.issue!;
+
+    const stateAfterDialogClose = {
+      open: false,
+      titleError: null,
+      descriptionError: null,
+      assigneeError: null,
+      charactersLeft: issueConstants.descriptionMaxChars - description.length,
+      title,
+      description,
+      assignee,
+      status,
+    };
+
+    this.setState(stateAfterDialogClose);
   };
 
   handleClickOpen = (): void => {
@@ -335,7 +396,6 @@ class CreateEditIssue extends Component<
 }
 
 const mapStateToProps = (state: RootState) => ({
-  issues: state.issues,
   users: state.users,
 });
 
--- src/components/Issue/CreateEditIssue/EditIssue/EditIssue.tsx
+++ src/components/Issue/CreateEditIssue/EditIssue/EditIssue.tsx
@@ -1,7 +1,6 @@
 import { Component } from "react";
 import { editIssue } from "common/actions";
 import { IIssue } from "common/models";
-import { issueConstants, Status } from "common/constants";
 import CreateEditIssue from "components/Issue/CreateEditIssue/CreateEditIssue";
 import { AppDispatch } from "store/store";
 import { connect } from "react-redux";
@@ -11,25 +10,12 @@ interface EditIssueProps {
   issue: IIssue;
 }
 
-interface ICloseEditIssueDialog {
-  open: boolean;
-  isSubmitting: boolean;
-  titleError: null;
-  descriptionError: null;
-  charactersLeft: number;
-  title: string;
-  description: string;
-  assignee: string;
-  status: string;
-}
-
 class EditIssue extends Component<EditIssueProps> {
   render() {
     return (
       <CreateEditIssue
         issue={this.props.issue}
         successAction={this.handleSubmit}
-        cancelAction={this.handleClose}
         isEditing={true}
       ></CreateEditIssue>
     );
@@ -38,27 +24,8 @@ class EditIssue extends Component<EditIssueProps> {
   handleSubmit = (issue: IIssue): void => {
     const { id, title, description, assignee, status } = issue;
     this.props.dispatch(
-      editIssue({ id, title, description, assignee, status: status as Status })
+      editIssue({ id, title, description, assignee, status })
     );
-  };
-
-  handleClose = (
-    title: string,
-    description: string,
-    assignee: string,
-    status: string
-  ): ICloseEditIssueDialog => {
-    return {
-      open: false,
-      isSubmitting: false,
-      titleError: null,
-      descriptionError: null,
-      charactersLeft: issueConstants.descriptionMaxChars - description.length,
-      title,
-      description,
-      assignee,
-      status,
-    };
   };
 }
 
