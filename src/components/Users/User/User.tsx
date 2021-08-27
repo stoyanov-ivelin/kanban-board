d44712997cf2da5c46a875e6e4d27c620379a0bb
Ivelin Stoyanov <ivelin.stoyanov@atscale.com>
2021-08-27 15:33:13 +0300

    Add edit button to User Card component

--- src/components/Users/User/User.tsx
+++ src/components/Users/User/User.tsx
@@ -11,14 +11,26 @@ import {
   Typography,
 } from "@material-ui/core";
 import { IUser } from "common/models";
+import EditUser from "components/CreateEditUser/EditUser/EditUser";
 import { Component } from "react";
 import "./User.css";
 
-interface UsersProps {
+interface UserProps {
   user: IUser;
 }
 
-class User extends Component<UsersProps> {
+interface UserState {
+  showEditButton: boolean;
+}
+
+class User extends Component<UserProps, UserState> {
+  constructor(props: UserProps) {
+    super(props);
+
+    this.state = {
+      showEditButton: false,
+    };
+  }
   render() {
     const { id, profilePicture, name, jobPosition, description, skills } =
       this.props.user;
@@ -26,48 +38,73 @@ class User extends Component<UsersProps> {
     const skillsLimit = 3;
 
     return (
-        <Grid item key={id} xs={4}>
-          <Card variant="outlined" className="user-card">
+      <Grid item key={id} xs={4}>
+        <Card
+          variant="outlined"
+          className="user-card"
+          onMouseEnter={this.handleMouseEnter}
+          onMouseLeave={this.handleMouseLeave}
+        >
+          <div className="user-card-header">
             <CardHeader
-              className="user-card-header"
               avatar={<Avatar src={profilePicture} />}
               title={name}
               subheader={jobPosition}
             />
-            <CardContent>
-              <Tooltip arrow title={description}>
-                <Typography
-                  variant="body2"
-                  color="textPrimary"
-                  className="tooltip"
-                >
-                  {description}
-                </Typography>
-              </Tooltip>
-              {skills.map((skill, index) => {
-                return (
-                  <List dense key={index}>
-                    {index < skillsLimit && (
-                      <ListItem>
-                        <ListItemAvatar>
-                          <Avatar src={skill.img} />
-                        </ListItemAvatar>
-                        {skill.name}
-                      </ListItem>
-                    )}
-                    {index === skillsLimit && (
-                      <Tooltip arrow title={skills.slice(skillsLimit).map(skill => skill.name).join(", ")}>
-                        <div>... {skills.length - skillsLimit} more</div>
-                      </Tooltip>
-                    )}
-                  </List>
-                );
-              })}
-            </CardContent>
-          </Card>
-        </Grid>
+            {this.state.showEditButton && <EditUser user={this.props.user} />}
+          </div>
+          <CardContent>
+            <Tooltip arrow title={description}>
+              <Typography
+                variant="body2"
+                color="textPrimary"
+                className="tooltip"
+              >
+                {description}
+              </Typography>
+            </Tooltip>
+            {skills.map((skill, index) => {
+              return (
+                <List dense key={index}>
+                  {index < skillsLimit && (
+                    <ListItem>
+                      <ListItemAvatar>
+                        <Avatar src={skill.img} />
+                      </ListItemAvatar>
+                      {skill.name}
+                    </ListItem>
+                  )}
+                  {index === skillsLimit && (
+                    <Tooltip
+                      arrow
+                      title={skills
+                        .slice(skillsLimit)
+                        .map((skill) => skill.name)
+                        .join(", ")}
+                    >
+                      <div>... {skills.length - skillsLimit} more</div>
+                    </Tooltip>
+                  )}
+                </List>
+              );
+            })}
+          </CardContent>
+        </Card>
+      </Grid>
     );
   }
+
+  handleMouseEnter = () => {
+    this.setState({
+      showEditButton: true,
+    });
+  };
+
+  handleMouseLeave = () => {
+    this.setState({
+      showEditButton: false,
+    });
+  };
 }
 
 export default User;
