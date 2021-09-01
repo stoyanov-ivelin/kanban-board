8f41b24de8c9b5b3336deee7e78042db13f710db
Ivelin Stoyanov <ivelin.stoyanov@atscale.com>
2021-09-01 14:26:08 +0300

    Refactor Board component

---
+++ src/components/Boards/Board/Board.tsx
@@ -0,0 +1,100 @@
+import { Component, ReactNode } from "react";
+import { connect } from "react-redux";
+import "components/Boards/Board/Board.css";
+import { AppDispatch, RootState } from "store/store";
+import { IBoard, IIssue, IUser } from "common/models";
+import BoardColumn from "components/Boards/Board/BoardColumn/BoardColumn";
+import { Grid, Avatar, Divider } from "@material-ui/core";
+
+interface BoardProps {
+  groupBy: string;
+  selectedBoard: string;
+  issues: Array<IIssue>;
+  users: Array<IUser>;
+  boards: Array<IBoard>;
+  dispatch: AppDispatch;
+}
+
+class Board extends Component<BoardProps> {
+  render(): ReactNode {
+    const boardToRender = this.props.boards.find(
+      (board) => board.name === this.props.selectedBoard
+    );
+
+    return <>{this.renderBoard(boardToRender!)}</>;
+  }
+
+  renderBoard(boardToRender: IBoard) {
+    const { groupBy } = this.props;
+
+    switch (groupBy) {
+      case "":
+        return this.renderBoardNoGrouping(boardToRender);
+      case "assignee":
+        return this.renderBoardGroupByAssignee(boardToRender);
+    }
+  }
+
+  renderBoardGroupByAssignee(boardToRender: IBoard): JSX.Element {
+    return (
+      <>
+        {this.props.users.map((user) => (
+          <>
+            <Grid item container alignItems="center" justifyContent="center">
+              <Avatar src={user.profilePicture} />
+            <Grid item>
+              <h2>{user.name}</h2>
+            </Grid>
+            </Grid>
+            {boardToRender.columns.map((column) => {
+              const filteredIssues = this.props.issues.filter(
+                (issue) =>
+                  column.statuses.includes(issue.status.toLowerCase()) &&
+                  issue.assignee === user.name
+              );
+
+              return (
+                <BoardColumn
+                  title={column.name}
+                  status={column.statuses[0].toUpperCase()}
+                  count={filteredIssues.length}
+                  issues={filteredIssues}
+                  assignee={user}
+                />
+              );
+            })}
+          </>
+        ))}
+      </>
+    );
+  }
+
+  renderBoardNoGrouping(boardToRender: IBoard): JSX.Element {
+    return (
+      <>
+        {boardToRender.columns.map((column) => {
+          const filteredIssues = this.props.issues.filter((issue) =>
+            column.statuses.includes(issue.status.toLowerCase())
+          );
+
+          return (
+            <BoardColumn
+              title={column.name}
+              status={column.statuses[0].toUpperCase()}
+              count={filteredIssues.length}
+              issues={filteredIssues}
+            />
+          );
+        })}
+      </>
+    );
+  }
+}
+
+const mapStateToProps = (state: RootState) => ({
+  issues: state.issues,
+  users: state.users,
+  boards: state.boards,
+});
+
+export default connect(mapStateToProps)(Board);
