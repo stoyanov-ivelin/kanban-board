d1c2fc1d11e049ce87e0c88dec21d9138e779b5d
Ivelin Stoyanov <ivelin.stoyanov@atscale.com>
2021-09-01 14:25:45 +0300

    Refactor Boards component

--- src/components/Boards/Boards.tsx
+++ src/components/Boards/Boards.tsx
@@ -1,80 +1,68 @@
-import { Grid, Typography } from "@material-ui/core";
-import { Component } from "react";
-import { connect } from "react-redux";
+import { FormControl, Grid, NativeSelect, Typography } from "@material-ui/core";
+import React, { Component } from "react";
 import "components/Boards/Boards.css";
-import { Status } from "common/constants";
-import Board from "components/Board/Board";
-import { RootState } from "store/store";
-import { IIssue } from "common/models";
+import Board from "components/Boards/Board/Board";
+import { IBoard, IIssue, IUser } from "common/models";
 import CreateIssue from "components/Issue/CreateEditIssue/CreateIssue/CreateIssue";
-
-interface FilteredIssues {
-  new: Array<IIssue>;
-  inProgress: Array<IIssue>;
-  done: Array<IIssue>;
-};
 
 interface BoardsProps {
   issues: Array<IIssue>;
+  users: Array<IUser>;
+  boards: Array<IBoard>;
 }
 
-class Boards extends Component<BoardsProps> {
+interface BoardsState {
+  groupBy: string;
+  selectedBoard: string;
+}
+
+class Boards extends Component<BoardsProps, BoardsState> {
+  constructor(props: BoardsProps) {
+    super(props);
+
+    this.state = {
+      groupBy: "",
+      selectedBoard: "Default",
+    };
+  }
   render() {
-    const filteredIssues = this.props.issues.reduce(
-      (filteredIssues: FilteredIssues, issue: IIssue) => {
-        if (issue.status === Status.New) {
-          filteredIssues.new.push(issue);
-        } else if (issue.status === Status.InProgress) {
-          filteredIssues.inProgress.push(issue);
-        } else {
-          filteredIssues.done.push(issue);
-        }
+    const { groupBy, selectedBoard } = this.state;
 
-        return filteredIssues;
-      },
-      { new: [], inProgress: [], done: [] }
-    );
-
-    const newIssues = filteredIssues.new;
-    const inProgressIssues = filteredIssues.inProgress;
-    const doneIssues = filteredIssues.done;
     return (
       <div>
-          <Grid className="heading" container alignItems="center" justifyContent="flex-end">
-            <Grid item xs={7}>
-              <Typography variant="h3">Kanban Board</Typography>
-            </Grid>
-            <Grid item xs={3}>
-              <CreateIssue />
-            </Grid>
+        <Grid
+          container
+          className="heading"
+          alignItems="baseline"
+          justifyContent="space-between"
+        >
+          <Grid className="heading" item xs={3}>
+            <Typography variant="h3">Kanban Board</Typography>
+              <FormControl className="select-group-by">
+                <NativeSelect onChange={this.handleGroupBy}>
+                  <option value="">No grouping</option>
+                  <option value="assignee">Group by assignee</option>
+                </NativeSelect>
+              </FormControl>
           </Grid>
-        <Grid container justifyContent="center" spacing={10}>
-          <Board
-            title="New"
-            status={Status.New}
-            count={newIssues.length}
-            issues={newIssues}
-          />
-          <Board
-            title="In Progress"
-            status={Status.InProgress}
-            count={inProgressIssues.length}
-            issues={inProgressIssues}
-          />
-          <Board
-            title="Done"
-            status={Status.Done}
-            count={doneIssues.length}
-            issues={doneIssues}
-          />
+          <Grid item xs={5}>
+            <CreateIssue />
+          </Grid>
+          <Grid container spacing={8} className="board-grid-container">
+          <Board groupBy={groupBy} selectedBoard={selectedBoard}></Board>
+          </Grid>
         </Grid>
       </div>
     );
   }
+
+  handleGroupBy = (e: React.ChangeEvent<{ value: string }>) => {
+    const selectedGroupBy = e.target.value;
+
+    this.setState({
+      groupBy: selectedGroupBy,
+    });
+  };
 }
 
-const mapStateToProps = (state: RootState) => ({
-  issues: state.issues,
-});
-
-export default connect(mapStateToProps)(Boards);
+export default Boards;
