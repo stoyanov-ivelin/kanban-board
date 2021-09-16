54405763fad140e13e9718f917cde57953926889
Ivelin Stoyanov <ivelin.stoyanov@atscale.com>
2021-09-16 18:23:23 +0300

    Wrap SIdeNav in Grid component

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import AdminHome from "components/AdminHome/AdminHome";
 import Users from "components/Users/Users";
 import SideNav from "components/SideNav/SideNav";
 import IssueConfig from "components/IssueConfig/IssueConfig";
+import BoardsConfig from "components/BoardsConfig/BoardsConfig";
+import { Grid } from "@material-ui/core";
 
 const App = () => {
   const location = useLocation();
@@ -14,14 +16,21 @@ const App = () => {
   return (
     <div className="App">
       <NavBar />
+      <Grid container>
+        <Grid item xs={2}>
       {shouldRenderSideNav && <SideNav />}
+      </Grid>
+      <Grid item xs={shouldRenderSideNav ? 7 : 12}>
       <Switch>
         <Redirect path="/" exact to="/boards" />
         <Route path="/boards" component={Boards} />
         <Route exact path="/admin" component={AdminHome} />
         <Route path="/admin/users" component={Users} />
         <Route path="/admin/issue-config" component={IssueConfig} />
+        <Route path="/admin/boards" component={BoardsConfig} />
       </Switch>
+      </Grid>
+      </Grid>
     </div>
   );
 };
